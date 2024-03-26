6d1f191b4191332ade56ea28125fa4727bdbf541
Zurysabdai <Zurisabdai.nunez@iest.edu.mx>
2024-03-25 22:57:30 -0600

    second

---
+++ alumno.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="fondo_alumno">
+    <header class="header_alumno">
+        <div class="head_alumno">
+            <nav class="navegacion-principal">
+                <a class='exit-btn' href="login.php">Salir</a>
+            </div>
+        </div>
+    </header>
+    <aside>
+        <p> Menu </p>
+        <a href="#">
+          <i class="fa fa-user-o" aria-hidden="true"></i>
+          Inscripción
+        </a>
+        <a href="#">
+          <i class="fa fa-laptop" aria-hidden="true"></i>
+          Servicios
+        </a>
+      </aside>
+      
+      <div class="social">
+        <a href="https://www.linkedin.com/in/florin-cornea-b5118057/" target="_blank">
+          <i class="fa fa-linkedin"></i>
+        </a>
+      </div>
+</body>
+</html>
